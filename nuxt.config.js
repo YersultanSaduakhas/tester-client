9e3f586de3cafd9d243f41e6acc70f5ee3252cd2
karnurmax <karnurmax@mail.ru>
2021-08-30 14:17:43 +0600

    rename site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - tester-client',
-    title: 'tester-client',
+    titleTemplate: '%s - Нұсқа талдау',
+    title: 'Нұсқа талдау',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
